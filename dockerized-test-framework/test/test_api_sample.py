71e2ba384596b450f1fe1831cb369b0ba6850211
syedsaqib <engr.saqi14@gmail.com>
2026-05-30 03:07:50 +0200

    Updates

---
+++ dockerized-test-framework/test/test_api_sample.py
@@ -0,0 +1,37 @@
+yimport pytest
+import requests
+
+BASE_URL = "https://jsonplaceholder.typicode.com"
+
+@pytest.mark.smoke
+def test_get_users():
+
+    response = requests.get(f"{BASE_URL}/users")
+
+    assert response.status_code == 200
+
+
+@pytest.mark.regression
+def test_get_single_user():
+
+    response = requests.get(f"{BASE_URL}/users/1")
+
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+
+
+@pytest.mark.regression
+def test_create_post():
+
+    payload = {
+        "title": "pytest",
+        "body": "testing",
+        "userId": 1
+    }
+
+    response = requests.post(
+        f"{BASE_URL}/posts",
+        json=payload
+    )
+
+    assert response.status_code == 201
